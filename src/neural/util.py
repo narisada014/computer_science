35b509d4213bbd661b74fd9226459ce9ed77beae
narisada014 <nari.reishi@gmail.com>
2019-10-17 02:38:28 +0900

    how to sigmoid derivative comment

--- src/neural/util.py
+++ src/neural/util.py
@@ -9,7 +9,7 @@ def dot_product(xs: List[float], ys: List[float]) -> float:
 def sigmoid(x: float) -> float:
     return 1.0 / (1.0 + exp(-x))
 
-
+# 参考：http://nonbiri-tereka.hatenablog.com/entry/2014/06/30/134023
 def derivative_sigmoid(x: float) -> float:
     sig: float = sigmoid(x)
     return sig * (1 - sig)
